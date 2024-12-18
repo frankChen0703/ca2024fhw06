63fbef162c700b379acad722a10061258561a993
frankChen0703 <115651052+frankChen0703@users.noreply.github.com>
2024-12-18 19:39:54 +0800

    Updata

---
+++ Assets/video.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.Video;
+
+
+public class video: MonoBehaviour
+{
+    public VideoPlayer videoPlayer;
+    public AudioSource audioSource;
+
+    private void Awake()
+    {
+        videoPlayer.Play();
+        audioSource.Play();
+    }
+}
